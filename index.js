29afe603e8991ea70483601f94c19557df6ce9ef
hajun <audgkwns44@gmail.com>
2023-05-14 00:16:10 +0900

    [Update]remove ip address for ec2

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ import { app } from "./src/app.js";
 dotenv.config();
 console.log(process.env.SERVER_PORT);
 
-const PORT = process.env.SERVER_PORT || 8000;
-const IP = process.env.SERVER_IP || "localhost";
+const PORT = process.env.SERVER_PORT || 3001;
 
-app.listen(PORT, IP, () => {
+app.listen(PORT, () => {
   console.log(`SERVER IS WORKING NOW : http://localhost:${PORT}/`);
-  console.log(`LOCAL ACCESS AVAILABLE ON : http://${IP}:${PORT}/`);
 });
